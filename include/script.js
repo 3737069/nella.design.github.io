425923cb2c626b62db8d76e69a54713095d6b625
3737069 <66836085+3737069@users.noreply.github.com>
2022-06-19 02:10:18 +0800

    Update script.js

--- include/script.js
+++ include/script.js
@@ -1,24 +1,36 @@
-for (let el of document.querySelectorAll(".hover")) {
-  let image = new Image();
-  image.src = el.dataset.src;
-  image.className = "followMouse";
-  image.style.width = `auto`;
+function mediaQueryCheck(inputQuery) {
+  var content = document.getElementById("navigation");
+  if (inputQuery.matches) {
+    for (let el of document.querySelectorAll(".hover")) {
+      let image = new Image();
+      image.src = el.dataset.src;
+      image.className = "followMouse";
+      image.style.width = "250px";
+      image.style.position = "fixed";
 
-  el.addEventListener("mouseover", (e) => {
-    document.getElementById("container").append(image);
-    image.style.left = `${e.clientX - image.width / 2}px`;
-    image.style.top = `${e.clientY - image.height * 1.5}px`;
-  });
+      el.addEventListener("mouseover", (e) => {
+        document.getElementById("container").append(image);
+        image.style.left = `${e.clientX * 1.1}px`;
+        image.style.top = `${e.clientY * 1.05}}px`;
+        console.log("mouseover has been activate", e.clientX, e.clientY);
+      });
 
-  el.addEventListener("mouseout", (e) => {
-    image.remove();
-  });
-}
+      el.addEventListener("mouseout", (e) => {
+        image.remove();
+      });
+    }
 
-window.addEventListener("mousemove", (e) => {
-  let image = document.querySelector(".followMouse");
-  if (image) {
-    image.style.left = `${e.clientX - image.width / 2}px`;
-    image.style.top = `${e.clientY - image.height * 1.5}px`;
+    window.addEventListener("mousemove", (e) => {
+      let image = document.querySelector(".followMouse");
+      if (image) {
+        image.style.left = `${e.clientX * 1.1}px`;
+        image.style.top = `${e.clientY * 1.05}px`;
+        console.log("mousemove has been activate", e.clientX, e.clientY);
+      }
+    });
+  } else {
   }
-});
+}
+var mobileQuery = window.matchMedia("(min-width: 900px)");
+mediaQueryCheck(mobileQuery);
+mobileQuery.addListener(mediaQueryCheck);
