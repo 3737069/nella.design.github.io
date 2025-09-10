c7e3c304ae74882e7ee2d44fbf427a1960539d8e
N <66836085+3737069@users.noreply.github.com>
2025-09-10 17:43:35 +0800

    Update script.js

--- include/script.js
+++ include/script.js
@@ -24,39 +24,3 @@ function copyEmail(element) {
       }, 2000);
     });
 }
-
-//hover img//
-function mediaQueryCheck(inputQuery) {
-  var content = document.getElementById("navigation");
-  if (inputQuery.matches) {
-    for (let el of document.querySelectorAll(".hover")) {
-      let image = new Image();
-      image.src = el.dataset.src;
-      image.className = "followMouse";
-      image.style.width = "240px";
-      image.style.position = "fixed";
-
-      el.addEventListener("mouseover", (e) => {
-        document.getElementById("container").append(image);
-        image.style.left = `${e.clientX * 1.1}px`;
-        image.style.top = `${e.clientY * 1.05}}px`;
-      });
-
-      el.addEventListener("mouseout", (e) => {
-        image.remove();
-      });
-    }
-
-    window.addEventListener("mousemove", (e) => {
-      let image = document.querySelector(".followMouse");
-      if (image) {
-        image.style.left = `${e.clientX * 1.1}px`;
-        image.style.top = `${e.clientY * 1.05}px`;
-      }
-    });
-  } else {
-  }
-}
-var mobileQuery = window.matchMedia("(min-width: 900px)");
-mediaQueryCheck(mobileQuery);
-mobileQuery.addListener(mediaQueryCheck);
